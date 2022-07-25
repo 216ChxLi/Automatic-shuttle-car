860ea41a8374b77e1ff794d67dedfcde96f97738
216ChxLi <92917022+216ChxLi@users.noreply.github.com>
2022-07-25 10:43:28 +0200

    Color Detection based on HSV

--- HSV_Color.cpp
+++ HSV_Color.cpp
@@ -1,17 +1,17 @@
 	vector<Vec4d> setWarnings_HSV()
     {
         cv::Scalar scalarL = cv::Scalar( 0, 90, 90 );
-        cv::Scalar scalarH = cv::Scalar( 30, 255, 255 );	
-	//cv::Scalar scalarL = cv::Scalar( 26, 43, 46 );
-        //cv::Scalar scalarH = cv::Scalar( 34, 255, 255 );
+        cv::Scalar scalarH = cv::Scalar( 25, 255, 255 );
+        // H82 S185 V30			H129 S212 V37		H152 S155 V33
 
-		cv::Mat img_hsv;
+        cv::Mat img_hsv;
         cv::cvtColor( image_, img_hsv, COLOR_BGR2HSV );
         cv::inRange( img_hsv, scalarL, scalarH, img_hsv );
         imshow( "HSV", img_hsv );
 
         vector<Vec4d> warning_lanes;  // will hold all the results of the detection
-        //HoughLinesP( img_threshold, warning_lanes, 1, CV_PI / 180, 50, 50, 10 );  // Probabilistic Line Transform
+        // HoughLinesP( img_threshold, warning_lanes, 1, CV_PI / 180, 50, 50, 10 );  //
+        // Probabilistic Line Transform
 
         return warning_lanes;
     }
